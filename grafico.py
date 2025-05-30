29583efb34f48e4bcc0b8f3b40fe66b65ca457b5
juliafernandes55 <fjulia174@gmail.com>
2025-05-30 09:59:40 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -9,8 +9,8 @@ st. subheader ("Dados Carregados")
 st. write(df.head())
                  
 colunas = df.columns.tolist()
-x_col = st. selectbox("Escolha a coluna para o eixo X (categorias)", colunas)
-y_col = st-selectbox("Escolha a coluna para o eixo Y (valores)", colunas)
+x_col = st.selectbox("Escolha a coluna para o eixo X (categorias)", colunas)
+y_col = st.selectbox("Escolha a coluna para o eixo Y (valores)", colunas)
 
-st. subheader ("Gráfico de Barras")
+st.subheader ("Gráfico de Barras")
 st.bar_chart (data-df, x=x_col, y=y_col, use_container_width=True)
